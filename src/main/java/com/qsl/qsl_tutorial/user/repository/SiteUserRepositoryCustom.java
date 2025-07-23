418579877f915a38e6778c27a2a154e1da6fc73c
pty10510 <pty10510@gmail.com>
2025-07-23 21:30:12 +0900

    feat : queryDSL 메서드 생성

--- src/main/java/com/qsl/qsl_tutorial/user/repository/SiteUserRepositoryCustom.java
+++ src/main/java/com/qsl/qsl_tutorial/user/repository/SiteUserRepositoryCustom.java
@@ -1,4 +1,7 @@
 package com.qsl.qsl_tutorial.user.repository;
 
+import com.qsl.qsl_tutorial.user.entity.SiteUser;
+
 public interface SiteUserRepositoryCustom {
+	SiteUser getQslUser(Long id);
 }
--- src/main/java/com/qsl/qsl_tutorial/user/repository/SiteUserRepositoryImpl.java
+++ src/main/java/com/qsl/qsl_tutorial/user/repository/SiteUserRepositoryImpl.java
@@ -1,4 +1,27 @@
 package com.qsl.qsl_tutorial.user.repository;
 
+import com.qsl.qsl_tutorial.user.entity.QSiteUser;
+import com.qsl.qsl_tutorial.user.entity.SiteUser;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class SiteUserRepositoryImpl implements SiteUserRepositoryCustom {
+
+	private final JPAQueryFactory jpaQueryFactory;
+
+	@Override
+	public SiteUser getQslUser(Long id) {
+		/*
+		SELECT * FROM site_user WHERE id = 1
+		*/
+		QSiteUser siteUser = QSiteUser.siteUser; // Q클래스가 있기 때문에 사용 가능
+
+
+		return jpaQueryFactory
+			.selectFrom(siteUser) // SELECT * FROM site_user
+			.where(siteUser.id.eq(id)) // WHERE id = 1
+			.fetchOne(); // 단일 결과를 반환, 결과가 없으면 null 반환
+	}
 }
